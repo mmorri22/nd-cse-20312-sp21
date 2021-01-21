beb7190ef2238b78f431153295c9ecc6f05c97ce
Matthew Morrison <mmorri22@nd.edu>
2021-01-21 12:18:57 -0500

    Lab 03 Update

--- Lab03/part1/fib.cpp
+++
@@ -1,8 +0,0 @@
-#include "fib.h"
-
-int Fibonacci::get(int n) {
-	if (n == 1 || n == 2) {
-		return 0;
-	}
-	return get(n - 1) + get(n - 2);	
-}
--- Lab03/part1/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "fib.h"
-#include "gtest/gtest.h"
-
-TEST(FibTest, Nominal) {
-	Fibonacci f;
-	EXPECT_EQ(5, f.get(5));
-	EXPECT_EQ(13, f.get(7));
-}
