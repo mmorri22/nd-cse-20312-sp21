3c683cf3bf82b2ad06a176f97722a8709aef256e
Matthew Morrison <mmorri22@nd.edu>
2021-02-13 21:28:11 -0500

    Lab 02 Final

--- Lab02/decoder.cpp
+++ Lab02/decoder.cpp
@@ -1,55 +1,50 @@
 #include <iostream>
 #include <string>
 
-// taken from http://stackoverflow.com/questions/3381614/c-convert-string-to-hexadecimal-and-vice-versa
-std::string string_to_hex(const std::string & input)
-{
-    static const char* const lut = "0123456789ABCDEF";
-    size_t len = input.length();
-
-    std::string output;
-    output.reserve(2 * len);
-    for (size_t i = 0; i < len; ++i)
-    {
-        const unsigned char c = input[i];
-        output.push_back(lut[c >> 4]);
-        output.push_back(lut[c & 15]);
-    }
-    return output;
-}
 
 // recursive function to reverse a string
 // you should start your debugging here with a breakpoint
-std::string ReverseText(std::string & msg, int start = 0, int end = -1)
+void ReverseText(std::string & msg, int start = 0, int end = -1)
 {
-	if (end  ==  -1) end = (int)msg.length() - 1;  // init case
-	if (end < start) return msg;             // base case
+	
+	if (end  ==  -1) 
+		end = (int)msg.length() - 1;  // init case
+	
+	if (end < start) 
+		return;             // base case
+	
 	else
 	{
-		char hold  = msg[start];
+		char swap_char  = msg[start];
 		msg[start] = msg[end];
-		msg[end]   = hold;
-		return ReverseText(msg, start++, end--);
+		msg[end]   = swap_char;
+		
+		ReverseText(msg, start++, end--);
 	}
 }
 
 // A simple loop-based decode function for string
-std::string DecodeText(std::string & msg)
-{
-	size_t len = msg.length();
-	for(size_t i = 0; i < len; ++i) msg[i] = msg[i] ^ 0x02;
-	return msg;
+void DecodeText(std::string & msg){
+	
+	long unsigned int len = msg.length();
+	
+	for(long unsigned int i = 0; i < len; ++i) 
+		msg[i] = msg[i] ^ 0x02;
+	
 }
 
 // decodes a secret message
 int main()
 {	
 	// Do not change this secret text
-	std::string secret = "novj,vgpagq]02`cn-q`cn-623qa-wfg,aqw,qvk`";
+	std::string secret = "qoSmSOjEqa5?t=javcu-oma,g`wvwm{,uuu--8qrvvj";
 	std::cout << "secret:  " << secret << std::endl;
-	std::string rev = ReverseText(secret);
-	std::cout << "Reverse: " << rev << std::endl;
-	std::string msg = DecodeText(secret);
-	std::cout << "Message: " << msg << std::endl;
+	
+	ReverseText(secret);
+	std::cout << "Reverse: " << secret << std::endl;
+	
+	DecodeText(secret);
+	std::cout << "Message: " << secret << std::endl;
+	
 	return 0;
 }
