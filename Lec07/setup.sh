6a67db4ee561fd03aeb8557ce011817c9d3c733a
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 17:18:02 -0500

    Lec 07 Update

--- Lec07/setup.sh
+++ Lec07/setup.sh
@@ -1,3 +1,28 @@
-wget wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/test.sh
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/test.sh
 chmod a+rx test.sh
 mkdir src inc exe obj
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/Makefile
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/inc/addr.h
+cp addr.h inc/addr.h 
+rm addr.h
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/inc/DLList.h
+cp DLList.h inc/DLList.h 
+rm DLList.h 
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/inc/student.h
+cp student.h inc/student.h 
+rm student.h
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/src/addr.cpp
+cp addr.cpp src/addr.cpp 
+rm addr.cpp
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/src/addr_test.cpp
+cp addr_test.cpp src/addr_test.cpp 
+rm addr_test.cpp
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/inc/DLList_Test.cpp
+cp DLList_Test.cpp inc/DLList_Test.cpp 
+rm DLList_Test.cpp 
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/src/student.cpp
+cp student.cpp src/student.cpp 
+rm student.cpp
+wget https://raw.githubusercontent.com/mmorri22/nd-cse-20312-sp21/master/Lec07/src/student_test.cpp
+cp student_test.cpp src/student_test.cpp 
+rm student_test.cpp
